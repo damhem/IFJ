4c1f09ebf2241c91b116bfce5f4ec37ff4963d84
zazakmazak <zdenekkroca256@seznam.cz>
2019-11-26 16:26:44 +0100

    early build verze, možná to i funguje?

--- symtable.h
+++ symtable.h
@@ -1,22 +1,33 @@
-#ifndef IFJ_SYMTABLE_H
-#define IFJ_SYMTABLE_H
+#ifndef SYMTABLE
+#define SYMTABLE
+
+#include <stdbool.h>
+#include "string.h"
 
 typedef enum {
-    Variable,     
-    Function,     
-} DataType;
+    Variable,
+    Function,
+} NodeType;
 
 typedef struct tBSTNode {
-    char* Key;			                             
-    DataType nodeDataType;                      
-    void* Data;                                     
-    struct tBSTNode * lPtr;                          
-    struct tBSTNode * rPtr;                          
+    char* Key;
+    NodeType DataType;
+    int VarType;
+    bool declared;
+    int parametrs;
+    string paramName[40];
+    struct tBSTNode * lPtr;
+    struct tBSTNode * rPtr;
 } *tBSTNodePtr;
+
+typedef struct symtable {
+    tBSTNodePtr root;
+} symtable;
+
 
 void BSTInit   (tBSTNodePtr *);
 tBSTNodePtr BSTSearch (tBSTNodePtr, char*);
-void BSTInsert (tBSTNodePtr *, char*, void*, DataType);
+void BSTInsert (tBSTNodePtr *, char*, NodeType, int, bool, int);
 void BSTDelete (tBSTNodePtr *, char*);
 void BSTDispose(tBSTNodePtr *);
 
